fab628f411833d28ee54fed37dfc83530648a733
Celso Bonutti Filho <celso.bonuttif@gmail.com>
2020-08-17 20:00:24 -0300

    Adds missing operations

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
 use byteorder::{BigEndian, LittleEndian, ReadBytesExt};
 
-fn main() {
-    let a: [u8; 2] = [0b11111111, 0b11111111];
-    let instruction = (&a[0..2]).read_u16::<BigEndian>().unwrap();
-    let pos = 0;
-
-    println!("{}", instruction);
-    println!("{}", a[0]);
-    println!("{:?}", &a[pos..pos+2]);
-}
+fn main() {}
--- src/xipe/instructions.rs
+++ src/xipe/instructions.rs
@@ -11,19 +11,40 @@ enum Instruction {
     register: u8,
     value: u8
   },
-  SkipIfRegisterEqual {
-    register_one: u8,
-    register_two: u8
-  },
+  SkipIfRegisterEqual(u8, u8),
   SetRegister {
-    register: u8,
+    target_register: u8,
     value: u8
   },
   IncreaseRegister {
-    register: u8,
+    target_register: u8,
     value: u8
   },
-  
+  SetByRegister(u8, u8),
+  SetXOrY(u8, u8),
+  SetXAndY(u8, u8),
+  SetXXORY(u8, u8),
+  AddsYToX(u8, u8),
+  SubsYFromX(u8, u8),
+  ShiftRight(u8),
+  SetXAsYMinusX(u8, u8),
+  ShiftLeft(u8),
+  SkipIfRegisterDifferent(u8, u8),
+  SetsITo(u16),
+  JumpsToNPlusV0(u16),
+  Random(u8, u8),
+  Display(u8, u8, u8),
+  SkipIfKeyPressed(u8),
+  SkipIfKeyNotPressed(u8),
+  SetXAsDelay(u8),
+  SetXAsKey(u8),
+  SetDelayAsX(u8),
+  SetSoundAsX(u8),
+  AddsXToI(u8),
+  SetsIAsSprite(u8),
+  StoreBCD(u8),
+  DumpRegisters(u8),
+  LoadRegisters(u8)
 }
 
 pub fn decode(op_code: u16) {
