8afb61465162627eb33256cfa783a4ea1f379c31
Celso Bonutti Filho <celso.bonuttif@gmail.com>
2020-08-18 00:22:53 -0300

    Renames bX to cX

--- src/xipe/instructions.rs
+++ src/xipe/instructions.rs
@@ -77,10 +77,10 @@ fn as_ts_pair(x: char, y: char) -> TargetSourcePair {
     }
 }
 
-fn as_rv_pair(register: char, b1: char, b2: char) -> RegisterValuePair {
+fn as_rv_pair(register: char, c1: char, c2: char) -> RegisterValuePair {
     RegisterValuePair {
         register: hex_char_to_integer!(register),
-        value: hex_char_to_integer!(b1, b2),
+        value: hex_char_to_integer!(c1, c2),
     }
 }
 
@@ -92,13 +92,13 @@ pub fn decode(op_code: u16) -> Instruction {
     match bits_array {
         ['0', '0', 'E', '0'] => Instruction::ClearDisplay,
         ['0', '0', 'E', 'E'] => Instruction::Return,
-        ['1', b1, b2, b3] => Instruction::GoTo(hex_char_to_integer!(b1, b2, b3)),
-        ['2', b1, b2, b3] => Instruction::Call(hex_char_to_integer!(b1, b2, b3)),
-        ['3', register, b1, b2] => Instruction::SkipIfEqual(as_rv_pair(register, b1, b2)),
-        ['4', register, b1, b2] => Instruction::SkipIfDifferent(as_rv_pair(register, b1, b2)),
+        ['1', c1, c2, c3] => Instruction::GoTo(hex_char_to_integer!(c1, c2, c3)),
+        ['2', c1, c2, c3] => Instruction::Call(hex_char_to_integer!(c1, c2, c3)),
+        ['3', register, c1, c2] => Instruction::SkipIfEqual(as_rv_pair(register, c1, c2)),
+        ['4', register, c1, c2] => Instruction::SkipIfDifferent(as_rv_pair(register, c1, c2)),
         ['5', x, y, '0'] => Instruction::SkipIfRegisterEqual(as_ts_pair(x, y)),
-        ['6', register, b1, b2] => Instruction::SetRegisterValue(as_rv_pair(register, b1, b2)),
-        ['7', register, b1, b2] => Instruction::AddToRegisterValue(as_rv_pair(register, b1, b2)),
+        ['6', register, c1, c2] => Instruction::SetRegisterValue(as_rv_pair(register, c1, c2)),
+        ['7', register, c1, c2] => Instruction::AddToRegisterValue(as_rv_pair(register, c1, c2)),
         ['8', x, y, '0'] => Instruction::AssignVYToVX(as_ts_pair(x, y)),
         ['8', x, y, '1'] => Instruction::SetXOrY(as_ts_pair(x, y)),
         ['8', x, y, '2'] => Instruction::SetXAndY(as_ts_pair(x, y)),
@@ -109,9 +109,9 @@ pub fn decode(op_code: u16) -> Instruction {
         ['8', x, y, '7'] => Instruction::SetXAsYMinusX(as_ts_pair(x, y)),
         ['8', x, _, 'E'] => Instruction::ShiftLeft(hex_char_to_integer!(x)),
         ['9', x, y, '0'] => Instruction::SkipIfRegisterDifferent(as_ts_pair(x, y)),
-        ['A', b1, b2, b3] => Instruction::SetIAs(hex_char_to_integer!(b1, b2, b3)),
-        ['B', b1, b2, b3] => Instruction::JumpToNPlusV0(hex_char_to_integer!(b1, b2, b3)),
-        ['C', register, b1, b2] => Instruction::Random(as_rv_pair(register, b1, b2)),
+        ['A', c1, c2, c3] => Instruction::SetIAs(hex_char_to_integer!(c1, c2, c3)),
+        ['B', c1, c2, c3] => Instruction::JumpToNPlusV0(hex_char_to_integer!(c1, c2, c3)),
+        ['C', register, c1, c2] => Instruction::Random(as_rv_pair(register, c1, c2)),
         ['D', x, y, height] => Instruction::Draw {
             x: hex_char_to_integer!(x),
             y: hex_char_to_integer!(y),
