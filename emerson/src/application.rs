dd3918035727df14ecd36bd84a3d655fa934d81d
Celso Bonutti Filho <celso.bonuttif@gmail.com>
2020-10-22 16:39:04 -0300

    Adds input

--- emerson/src/application.rs
+++ emerson/src/application.rs
@@ -1,6 +1,11 @@
 use iced::time;
 use iced::{
-  executor, window, Application, Column, Command, Container, Element, Settings, Subscription, Color
+  executor,
+  keyboard::{
+    Event::{KeyPressed, KeyReleased},
+    KeyCode,
+  },
+  window, Application, Color, Column, Command, Container, Element, Settings, Subscription,
 };
 use palmer::Chip8;
 use std::fs::File;
@@ -17,10 +22,33 @@ struct Emerson {
   cartridge_loaded: bool,
 }
 
+fn get_key(key: KeyCode) -> Option<usize> {
+  match key {
+    KeyCode::Key1 => Some(0x1),
+    KeyCode::Key2 => Some(0x2),
+    KeyCode::Key3 => Some(0x3),
+    KeyCode::Key4 => Some(0xC),
+    KeyCode::Q => Some(0x4),
+    KeyCode::W => Some(0x5),
+    KeyCode::E => Some(0x6),
+    KeyCode::R => Some(0xD),
+    KeyCode::A => Some(0x7),
+    KeyCode::S => Some(0x8),
+    KeyCode::D => Some(0x9),
+    KeyCode::F => Some(0xE),
+    KeyCode::Z => Some(0xA),
+    KeyCode::X => Some(0x0),
+    KeyCode::C => Some(0xB),
+    KeyCode::V => Some(0xF),
+    _ => None,
+  }
+}
+
 #[derive(Debug, Clone)]
 enum Message {
   Tick(Instant),
   Display(grid::Message),
+  Event(iced_native::Event),
 }
 
 pub fn run() -> iced::Result {
@@ -44,11 +72,11 @@ impl Application for Emerson {
   type Flags = ();
 
   fn scale_factor(&self) -> f64 {
-      10.
+    10.
   }
 
   fn background_color(&self) -> Color {
-      Color::BLACK
+    Color::BLACK
   }
 
   fn new(_flags: ()) -> (Self, Command<Message>) {
@@ -84,6 +112,28 @@ impl Application for Emerson {
           .update(grid::Message::Show(self.engine.display.pixels))
       }
       Message::Display(_) => (),
+      Message::Event(event) => match event {
+        iced_native::Event::Keyboard(event) => match event {
+          KeyPressed {
+            key_code,
+            modifiers: _,
+          } => {
+            if let Some(key) = get_key(key_code) {
+              self.engine.input.key_down(key)
+            }
+          }
+          KeyReleased {
+            key_code,
+            modifiers: _,
+          } => {
+            if let Some(key) = get_key(key_code) {
+              self.engine.input.key_up(key)
+            }
+          }
+          _ => (),
+        },
+        _ => (),
+      },
     }
 
     Command::none()
@@ -91,7 +141,10 @@ impl Application for Emerson {
 
   fn subscription(&self) -> Subscription<Self::Message> {
     if self.cartridge_loaded {
-      time::every(Duration::from_millis(2 as u64)).map(Message::Tick)
+      Subscription::batch(vec![
+        time::every(Duration::from_millis(2 as u64)).map(Message::Tick),
+        iced_native::subscription::events().map(Message::Event),
+      ])
     } else {
       Subscription::none()
     }
